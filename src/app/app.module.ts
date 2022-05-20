c023b8a61cd4ef73e5ad6ac6474911378cab1f36
aminLuis <aminluisamin@gmail.com>
2022-05-20 15:56:33 -0500

    Creacion de componentes y routing funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { CategoriaComponent } from './components/categoria/categoria.component';
+import { ProductoComponent } from './components/producto/producto.component';
+
+const routes:Routes = [
+  {path: 'categoria-component', component:CategoriaComponent},
+  {path: 'producto-component', component: ProductoComponent}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    CategoriaComponent,
+    ProductoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
